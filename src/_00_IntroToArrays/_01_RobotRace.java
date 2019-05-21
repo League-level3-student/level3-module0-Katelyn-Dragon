4b0184f3bc166dd225ab4972b7d6b25c75673eac
League Student <league@iMac-26.attlocal.net>
2019-05-20 18:24:26 -0700

    r

--- src/_00_IntroToArrays/_01_RobotRace.java
+++ src/_00_IntroToArrays/_01_RobotRace.java
@@ -8,8 +8,12 @@ import org.jointheleague.graphical.robot.Robot;
 
 public class _01_RobotRace {
 	//1. make a main method
-	
+	public static void main(String[] args) {
+		
+	}
 		//2. create an array of 5 robots.
+Robot[] rob = new Robot[5];
+rob[0] = Robot;
 
 		//3. use a for loop to initialize the robots.
 
