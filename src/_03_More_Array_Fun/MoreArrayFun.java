704e471aedbd57acd0021e1e217b00676a596a87
League Student <league@iMac-37.attlocal.net>
2019-12-02 18:22:52 -0800

    MAF

--- src/_03_More_Array_Fun/MoreArrayFun.java
+++ src/_03_More_Array_Fun/MoreArrayFun.java
@@ -5,17 +5,20 @@ import java.util.Random;
 public class MoreArrayFun {
 	//1. Create a main method to test the other methods you write.
 	public static void main(String[] args) {
-		
+	String[] arr = {"hi"};
+	printArray(arr);
 	}
 	
 	
 	//2. Write a method that takes an array of Strings and prints all the Strings in the array.
-	public static (String[] arr) {
-		System.out.println(arr);
+	public static void printArray (String[] arr) {
+		for (int i = 0; i < arr.length; i++) {
+			System.out.println(arr[i]);
+		}
 	}
 	//3. Write a method that takes an array of Strings and prints all the Strings in the array
 	//   in reverse order.
-	public static (String[] two) {
+	public static void Reverse (String[] two) {
 		
 	}
 	//4. Write a method that takes an array of Strings and prints every other String in the array.
